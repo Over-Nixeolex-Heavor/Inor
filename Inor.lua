d64af70fff30a30f9f3520d2a5954c74b0cb7821
Over Nixeolex Heavor <xahercoler@gmail.com>
2025-03-26 13:55:08 +0700

    Update Inor.lua

--- Inor.lua
+++ Inor.lua
@@ -1,3 +1,7 @@
+print("load")
+print("checking version")
+print("version 0.001")
+print("start")
 --[[ soon website and blacklist
 version: 0.001
 support: all mobile exploits and jjsploit xeno velocity zorara solara and private exploits
@@ -35,4 +39,14 @@ Tab:Button{
 	Name = "Teleport to King OF Hill",
 	Description = nil,
 	Callback = function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5219.84619, 1161.50635, -8881.83789, -0.579802752, -0.0825181007, -0.810567379, -0.188644648, 0.981420517, 0.0350269154, 0.792617142, 0.173217893, -0.584596872) end
+}
+Tab:Button{
+	Name = "Teleport to givers sword(rebirths need)",
+	Description = nil,
+	Callback = function() game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(5062.2876, 1118.70703, -8683.1875, -0.658802092, 0.0820137933, -0.747832537, -7.67526217e-07, 0.994040012, 0.109015703, 0.752316296, 0.0718203485, -0.654875576) end
+}
+GUI:Credit{
+	Name = "Credits by Nixeolex",
+	Description = "Creator inor",
+	Discord = "esterkor"
 }
